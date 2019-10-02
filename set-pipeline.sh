4682bf045c02610233f18486d557018011a424b4
Scott Brightwell <scott@brightwell.org>
2019-10-02 07:55:02 -0500

    updated ops manager vm

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -1,2 +1,2 @@
-fly -t hush-house set-pipeline -p pks-gcpdemo-scott -c pipeline.yml -l credentials.yml --verbose
-fly -t hush-house unpause-pipeline -p pks-gcpdemo-scott
+fly -t hush-house set-pipeline -p pks-gcplab-scott -c pipeline.yml -l credentials.yml --verbose
+fly -t hush-house unpause-pipeline -p pks-gcplab-scott
